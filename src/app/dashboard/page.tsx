fdf75c7e17bb7f53b369bd0f4a758b449e15006c
Vinícius Moura De Oliveira <vinicius.m2602@gmail.com>
2025-07-06 16:16:10 +0000

    Implemente a lógica de acompanhamento dos cronogramas com base nos orien

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -191,7 +191,7 @@ const ProfessorDashboard = () => {
         </CardHeader>
         <CardContent className="grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
           <QuickLink href="/dashboard/orientandos" icon={BookUser}>Visualizar Orientandos</QuickLink>
-          <QuickLink href="#" icon={CalendarCheck}>Acompanhar Cronogramas</QuickLink>
+          <QuickLink href="/dashboard/cronogramas" icon={CalendarCheck}>Acompanhar Cronogramas</QuickLink>
           <QuickLink href="#" icon={FileCheck2}>Aprovar/Rejeitar Relatórios</QuickLink>
         </CardContent>
       </Card>
